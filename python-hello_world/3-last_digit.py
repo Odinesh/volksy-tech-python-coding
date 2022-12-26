0d8a15dbcfab02ca8d04ec37b2660b9d5719530b
Odinesh <dinesho@brightcomgroup.com>
2022-12-26 10:21:26 +0000

    3-last_digit.py

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -10,4 +10,5 @@ else:
     if n > 5:
         print('Last digit of', number, 'is', n, 'and is greater than 5')
     else:
-        print('Last digit of', number, 'is', n, 'and is less than 6 and not 0') 
+        print('Last digit of', number, 'is', n, 'and is less than 6 and not 0')
+
